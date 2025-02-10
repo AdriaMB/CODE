f3b4da14b71f76ccc1ae358d21a9b478a0a64b94
AdriaMB <147397240+AdriaMB@users.noreply.github.com>
2025-02-10 21:19:11 +0100

    Goteras.c

--- Goteras.c
+++ Goteras.c
@@ -16,8 +16,14 @@ int main(void){
         int aux = c/60;
         MM = aux % 60;              //MM
         HH = aux / 60;              //HH
-
-        printf("%02d:%02d:%02d\n", HH, MM, SS);
+        
+        if(c >=86400){
+            printf("24:00:00\n");}
+        else if(c <= 0){
+            printf("00:00:00\n");}
+        else{
+            printf("%02d:%02d:%02d\n", HH, MM, SS);
+        }
 
         /*
          %02d: This format specifies that the value should be printed as an integer with at least two digits, padded with a leading zero if necessary. The 0 in the format specifier ensures padding with a zero.
