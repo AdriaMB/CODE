b0d2221a0d96c71eb6a9e86762b03e450bab2fcf
AdriaMB <147397240+AdriaMB@users.noreply.github.com>
2025-02-21 09:44:26 +0100

    Add files via upload
    Those days, many boys use beautiful girls' photos as avatars in forums. So it is pretty hard to tell the gender of a user at the first glance. Last year, our hero went to a forum and had a nice chat with a beauty (he thought so). After that they talked very often and eventually they became a couple in the network.
    
    But yesterday, he came to see "her" in the real world and found out "she" is actually a very strong man! Our hero is very sad and he is too tired to love again now. So he came up with a way to recognize users' genders by their user names.
    
    This is his method: if the number of distinct characters in one's user name is odd, then he is a male, otherwise she is a female. You are given the string that denotes the user name, please help our hero to determine the gender of this user by his method.
    Input
    
    The first line contains a non-empty string, that contains only lowercase English letters — the user name. This string contains at most 100 letters.
    Output
    
    If it is a female by our hero's method, print "CHAT WITH HER!" (without the quotes), otherwise, print "IGNORE HIM!" (without the quotes).

---
+++ BoyOrGirl.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class BoyOrGirl{
+    public static void main(String[]args){
+        Scanner in = new Scanner(System.in);
+        String name = in.nextLine();
+        char[] letters = new char[name.length()];
+        char aux;
+        int elem = 0;
+        int count = 0;
+
+        for(int i = 0; i < name.length(); i++){
+            aux = name.charAt(i);
+            if(!contains(aux, elem, letters)){
+                count++;
+            }
+        }
+
+        //IT WORKS!!!!!!!!!!
+        System.out.println(count%2==0 ? "CHAT WITH HER" :"IGNORE HIM");
+
+
+    }
+
+    public static boolean contains(char c, int elem, char[] theArray)
+    {
+        //Given a char, checks whether is contained in the first elements of theArray[]
+        for(int i = 0; i < elem; i++){
+            if(c == theArray[i]) return true;
+        }
+        return false;
+
+    }
+
+
+
+}
